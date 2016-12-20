52377df774218f47faf3d15269d276edc44aa5d0
Harutyun <harut_20@mail.ru>
2016-12-20 16:35:57 +0400

    Update config.php

--- config.php
+++ config.php
@@ -3,15 +3,15 @@ define("__PAYMENT_TEST__", 1);
 
 if (__PAYMENT_TEST__) {
 
-    define("MERCHANTNUMBER", "donate_test");
-    define('MERCHANTPASSWD', 'donate123');
+    define("MERCHANTNUMBER", "username");
+    define('MERCHANTPASSWD', 'password');
 
     define('TEST_URL', 'https://91.199.226.7/payment/rest/');
     define("REAL_PORT", "8445");
 } else {
 
-    define("MERCHANTNUMBER", "34536491_api");
-    define('MERCHANTPASSWD', 'donate123');
+    define("MERCHANTNUMBER", "username");
+    define('MERCHANTPASSWD', 'password');
 
     define('TEST_URL', 'https://ipay.arca.am/payment/rest/');
     define("REAL_PORT", "443");
